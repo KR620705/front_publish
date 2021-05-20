3f71ee462623ae2ff862c573be00c6ef0451091a
lee jaei <coma629@enders.co.kr>
2021-05-20 13:24:48 +0900

    검색 & 검색결과(nodata) 페이지와 css 내용 추가

--- acontents-front-vue/src/views/search/search_result_nodata.vue
+++ acontents-front-vue/src/views/search/search_result_nodata.vue
@@ -1,26 +1,20 @@
 <template>
-  <div class="search search">
+  <div class="search search_result_nodata">
     
     <section class="wrap_input">
         <button type="button" class="btn-search-close">닫기</button>
         <input type="text" class="search-input" placeholder="검색어를 입력하세요">
     </section>
 
-    <section class="recommand-word">
-      <div class="titlearea">
-				<h2 class="h2-title">추천 검색어</h2>
-			</div>
-      <ul>
-        <li><a href="javascript:void(0)">은퇴 후 자산관리</a></li>
-        <li><a href="javascript:void(0)">건강라디오</a></li>
-        <li><a href="javascript:void(0)">김남길 리더님</a></li>
-        <li><a href="javascript:void(0)">장수비법</a></li>
-        <li><a href="javascript:void(0)">브랜드 캠페인</a></li>
-        <li><a href="javascript:void(0)">암웨이 브랜드</a></li>
-        <li><a href="javascript:void(0)">디지털 삼총사</a></li>
-        <li><a href="javascript:void(0)">나만의 감성</a></li>
-        <li><a href="javascript:void(0)">나의 성공기</a></li>
-      </ul>
+    <section class="search-result">
+      <div class="search-result-text">
+        <h3 class="h3-title">검색 결과가 없습니다.</h3>
+        <p>
+          하단의 추천 해시태그를 통해서<br>
+          다른 콘텐츠들도 찾아보세요.
+        </p>
+      </div>
+      
     </section>
 
 		<section class="tag-list recommand">
@@ -47,7 +41,7 @@
 
 <script>
 export default {
-  name: 'Search',
+  name: 'Search_result',
   data: function () {
     return {
 
